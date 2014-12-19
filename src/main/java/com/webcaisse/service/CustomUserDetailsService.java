4e3475c25276aef1753720a2d8616078e0a73843
abdessalem <belhadj.abdessalem@gmail.com>
2014-12-19 23:28:52 +0100

    dev spring secrurity

--- src/main/java/com/webcaisse/service/CustomUserDetailsService.java
+++ src/main/java/com/webcaisse/service/CustomUserDetailsService.java
@@ -1,49 +1,48 @@
 package com.webcaisse.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.webcaisse.ws.interfaces.AuthentificationService;
 import com.webcaisse.ws.model.UserOut;
 
+public class CustomUserDetailsService implements UserDetailsService {
 
-public class CustomUserDetailsService implements UserDetailsService{
+	AuthentificationService authentificationService;
 
-	@Autowired(required=true)
-    AuthentificationService authentificationService ;
-	
-	
-	
-	public UserDetails loadUserByUsername(String userName)throws UsernameNotFoundException {
-	 
-		UserOut userOut = null ;
-		
-		
-		try{
+	public void setAuthentificationService(
+			AuthentificationService authentificationService) {
+		this.authentificationService = authentificationService;
+	}
+
+	public UserDetails loadUserByUsername(String userName)
+			throws UsernameNotFoundException {
+
+		UserOut userOut = null;
+
 		userOut = authentificationService.findByUserName(userName);
-		
-		
-		System.out.print(userOut.getNom());
-		System.out.print(userOut.getPrenom());
-		boolean enabled = true;
-		boolean accountNonExpired = true;
-		boolean credentialsNonExpired = true;
-		boolean accountNonLocked = true;
-		
-		return new User(userOut.getUsername(),userOut.getPassword(),enabled,accountNonExpired, credentialsNonExpired,accountNonLocked, null)  ;
-				
-		}catch(Exception e) {
-			throw new RuntimeException(e);
+
+		if (userOut != null) {
+			boolean enabled = true;
+			boolean accountNonExpired = true;
+			boolean credentialsNonExpired = true;
+			boolean accountNonLocked = true;
+			final List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+			authorities.add(new GrantedAuthorityImpl("ROLE_USER"));
+			return new User(userOut.getUsername(), userOut.getPassword(),
+					enabled, accountNonExpired, credentialsNonExpired,
+					accountNonLocked, authorities);
 		}
-	}
-	
+
+		throw new UsernameNotFoundException("User $username not found");
+
 	}
 
-	
-	
-
+}
