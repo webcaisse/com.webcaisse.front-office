49b96c9acc0484042312d5f2d1f63351f4c942b8
Khaled <sghaierkhaled@live.com>
2015-01-03 21:48:22 +0100

    dev parametre

--- src/main/java/com/webcaisse/mvc/controller/ParametragePageController.java
+++ src/main/java/com/webcaisse/mvc/controller/ParametragePageController.java
@@ -1,13 +1,31 @@
 package com.webcaisse.mvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.collections.FactoryUtils;
+import org.apache.commons.collections.ListUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.webcaisse.mvc.bean.Parametres;
+import com.webcaisse.service.CustomUser;
+import com.webcaisse.ws.interfaces.ParametreManagerService;
+import com.webcaisse.ws.model.ClientIn;
+import com.webcaisse.ws.model.ParametreIn;
 
 
 @Controller
 @RequestMapping("/parametrage")
 public class ParametragePageController {
+
+    @Autowired
+	ParametreManagerService parametreManagerService ;
 
 	@RequestMapping("/afficher")
 	public String afficher(){
@@ -15,8 +33,30 @@ public class ParametragePageController {
 		return "parametrage";
 	}
 	
-	@RequestMapping("/ajax/caisse")
+	@RequestMapping(value="/ajax/caisse", method = RequestMethod.GET)
 	public String afficherParametrageCaisse(ModelMap model){
+		//List<ParametreIn> parametres = new  ArrayList<ParametreIn>() ;
+
+        Parametres parametres = new Parametres () ;
+		model.addAttribute("parametres", parametres) ;
 		return "modules/parametrage/caisse";
 	}
+	
+	@RequestMapping(value="/sauvegarderParametres",method = RequestMethod.POST)
+	public void sauvegarderParametres (@ModelAttribute("parametres") Parametres parametres) 
+	{
+		CustomUser customUser = (CustomUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        
+		for (ParametreIn parametreIn : parametres.getParametres()) {
+			
+			parametreIn.setIdSociete(customUser.getSocieteId());
+			parametreManagerService.sauvegarderParametre(parametreIn);
+		}
+		
+		
+		
+		
+		
+	}
+	
 }
--- src/main/webapp/WEB-INF/freemarker/modules/parametrage/caisse.ftl
+++ src/main/webapp/WEB-INF/freemarker/modules/parametrage/caisse.ftl
@@ -1,1 +1,33 @@
-caisse
+
+<body>
+</br></br>
+
+<form action="${rc.getContextPath()}/parametrage/sauvegarderParametres" modelAttribute="parametres" method="POST">
+
+   <fieldset style="margin-bottom: 15px;padding: 10px;">
+      <legend style=" color: #384313;font-size: 16px;font-weight: bold;padding-bottom: 10px;text-shadow: 0 1px 1px #c0d576;">Sauvegarder Parametres</legend>
+     
+         <label style="display: block;width: 150px;float: left;" for="E1">Entite 1 ticket</label>
+          <input name="E1" path="parametre.nomParametre" value=""><br>
+          <label style="display: block;width: 150px;float: left;" for="E2">Entite 2 ticket</label>
+          <input name="E2"><br>
+          <label style="display: block;width: 150px;float: left;" for="E3">Entite 3 ticket</label>
+          <input name="E3"><br>
+          <label style="display: block;width: 150px;float: left;" for="E1">Entite 4 ticket</label>
+          <input name="E4"><br><br>
+          <label style="display: block;width: 150px;float: left;" for="P1">Pied 1 ticket</label>
+          <input name="P1"><br>
+           <label style="display: block;width: 150px;float: left;" for="P2">Pied 2 ticket</label>
+          <input name="P2"><br>
+           <label style="display: block;width: 150px;float: left;" for="P3">Pied 3 ticket</label>
+          <input name="P3"><br>
+           <label style="display: block;width: 150px;float: left;" for="P4">Pied 4 ticket</label>
+          <input name="P4"><br>
+           
+         
+    	<br>
+
+    </fieldset>
+   <p><input type="submit" value="Sauvegarder" style="float: left;"></p>
+</br></br>
+</form>
